7317287e7d4adba3605bf198d49c3e02c133b54d
Gage LeBlanc <gage.leblanc@optanix.com>
2020-09-20 19:15:11 -0400

    consolidating loader

--- clilib/util/loader.py
+++ clilib/util/loader.py
@@ -2,6 +2,9 @@ from pathlib import Path
 import json
 from sys import platform
 
+from clilib.util.arg_tools import arg_tools
+import importlib
+
 
 class Loader:
     @staticmethod
@@ -14,3 +17,18 @@ class Loader:
             with open(path) as f:
                 moduleSpec = json.load(f)
             return moduleSpec
+
+    @staticmethod
+    def start_app(modules, modules_base):
+        args, _ = arg_tools.command_parser(modules)
+        try:
+            module = importlib.import_module(modules_base + "." + args.command[0] + ".main")
+            module.main()
+        except ImportError as ex:
+            print("Command not found: " + args.command[0] + ". Command must be one of " + ", ".join(modules))
+            print(ex)
+        except SystemExit as _:
+            exit(0)
+        except KeyboardInterrupt as kbi:
+            print("User keyboard interrupt!")
+            exit(1)
