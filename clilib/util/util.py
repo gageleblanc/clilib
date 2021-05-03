cf33c41e33754333d7e3fe81f4ca6fbc632a5b15
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-03 12:32:12 -0400

    making some formatting changes, adding prompt

--- clilib/util/util.py
+++ clilib/util/util.py
@@ -1,29 +1,62 @@
 import logging
+import sys
+
 
 class Util:
-  @staticmethod
-  def dump_module(args, module):
-    if args.debug:
-      print(dir(module))
+    @staticmethod
+    def dump_module(args, module):
+        if args.debug:
+            print(dir(module))
 
-  @staticmethod
-  def import_test():
-    print("Import success!")
+    @staticmethod
+    def import_test():
+        print("Import success!")
 
-  @staticmethod
-  def configure_logging(args, name, fmt='[%(asctime)s][%(name)s][%(levelname)8s] - %(message)s'):
-    logFormatter = logging.Formatter(fmt=fmt)
-    log = logging.getLogger(name)
-    log.setLevel(logging.INFO)
-    if args.debug:
-      log.setLevel(logging.DEBUG)
+    @staticmethod
+    def configure_logging(args, name, fmt='[%(asctime)s][%(name)s][%(levelname)8s] - %(message)s'):
+        log_formatter = logging.Formatter(fmt=fmt)
+        log = logging.getLogger(name)
+        log.setLevel(logging.INFO)
+        if args.debug:
+            log.setLevel(logging.DEBUG)
 
-    consoleHandler = logging.StreamHandler()
-    consoleHandler.setLevel(logging.INFO)
-    consoleHandler.setFormatter(logFormatter)
-    if args.debug:
-      consoleHandler.setLevel(logging.DEBUG)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(logging.INFO)
+        console_handler.setFormatter(log_formatter)
+        if args.debug:
+            console_handler.setLevel(logging.DEBUG)
 
-    log.addHandler(consoleHandler)
+        log.addHandler(console_handler)
 
-    return log
+        return log
+
+    @staticmethod
+    def do_confirm(question, default="no"):
+        """Ask a yes/no question via raw_input() and return their answer.
+
+        "question" is a string that is presented to the user.
+        "default" is the presumed answer if the user just hits <Enter>.
+                It must be "yes" (the default), "no" or None (meaning
+                an answer is required of the user).
+
+        The "answer" return value is True for "yes" or False for "no".
+        """
+        valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+        if default is None:
+            prompt = " [y/n] "
+        elif default == "yes":
+            prompt = " [Y/n] "
+        elif default == "no":
+            prompt = " [y/N] "
+        else:
+            raise ValueError("invalid default answer: '%s'" % default)
+
+        while True:
+            sys.stdout.write(question + prompt)
+            choice = input().lower()
+            if default is not None and choice == "":
+                return valid[default]
+            elif choice in valid:
+                return valid[choice]
+            else:
+                sys.stdout.write("Please respond with 'yes' or 'no' " "(or 'y' or 'n').\n")
