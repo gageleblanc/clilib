64d74707638f3eaf01e81ff90ca5b747a55589f4
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-03 12:51:28 -0400

    updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='clilib',
-    version='1.8.1',
+    version='1.8.2',
     scripts=[],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
