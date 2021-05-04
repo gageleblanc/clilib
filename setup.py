1207373ac304cf6c0da89cb75776a77211329aa3
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-04 14:56:49 -0400

    move file, only run on branch for test pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='clilib',
-    version='1.9.5',
+    version='1.9.6',
     scripts=[],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
