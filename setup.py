357811302f40fad7ff44775cbbc7ac60393373c5
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-04 14:48:50 -0400

    separate build and publish

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='clilib',
-    version='1.9.3',
+    version='1.9.4',
     scripts=[],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
