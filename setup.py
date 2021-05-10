7404b18ef300010759f4776a28c12a36f4e24e56
Gage LeBlanc <gage.leblanc@optanix.com>
2021-05-10 19:45:34 -0400

    prevent multiple handlers

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='clilib',
-    version='1.9.8',
+    version='1.9.9',
     scripts=[],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
