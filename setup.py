7b35475fbfae38dc9a6d441a61a7480fc8660cad
Gage LeBlanc <gleblanc@symnet.io>
2021-07-13 23:24:09 -0400

    logging and config utils

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name='clilib',
-    version='1.9.12',
+    version='2.0.0',
     scripts=[],
     author="Gage LeBlanc",
     author_email="gleblanc@symnet.io",
