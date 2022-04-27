1f0303fd922d79d2601688f86c12a4af05d6a31c
Gage LeBlanc <gleblanc@symnet.io>
2022-04-27 19:26:53 -0400

    add an event manager

---
+++ clilib/events.py
@@ -0,0 +1,63 @@
+from clilib.util.logging import Logging
+import threading
+
+
+class EventManager:
+    """
+    Store and manage events and handlers
+    """
+    def __init__(self, debug: bool = False):
+        self.logger = Logging(log_name="clilib", log_desc="EventManager", debug=debug).get_logger()
+        self.event_handlers = {}
+
+    def __call__(self, event_name: str, event_data: dict):
+        """
+        Call event handlers
+        :param event_name: Event name to trigger
+        :param event_data: Data to pass to handlers
+        :return:
+        """
+        if not isinstance(event_data, dict):
+            self.logger.error("Invalid event data: {}".format(event_data))
+            return None
+        if event_name not in self.event_handlers:
+            self.logger.warn("Unknown event: {}".format(event_name))
+            return None
+        for handler in self.event_handlers[event_name]:
+            try:
+                handler(event_data)
+                return True
+            except Exception as e:
+                self.logger.error("Error calling handler: {}".format(e))
+
+    def on(self, event_name: str, handler):
+        """
+        Add an event handler
+        Alias for add
+        :param event_name: Event name
+        :param handler: Event handler
+        :return:
+        """
+        self.add(event_name, handler)
+
+    def add(self, event_name: str, handler):
+        """
+        Add an event handler
+        :param event_name: Event name
+        :param handler: Event handler
+        :return:
+        """
+        if event_name not in self.event_handlers:
+            self.event_handlers[event_name] = []
+        self.event_handlers[event_name].append(handler)
+
+    def remove(self, event_name: str, handler):
+        """
+        Remove an event handler
+        :param event_name: Event name
+        :param handler: Event handler
+        :return:
+        """
+        if event_name not in self.event_handlers:
+            return
+        self.event_handlers[event_name].remove(handler)
