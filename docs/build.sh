2a5e73a7c725ba10a4ef355720c1c230ad467a3c
Gage LeBlanc <gage.leblanc@rebuyengine.com>
2022-08-11 11:19:41 -0400

    update docs again because pdoc is not very good

--- docs/build.sh
+++ docs/build.sh
@@ -4,6 +4,7 @@ if [ -d "./html" ]; then
     source cleanup.sh
 fi
 
-pdoc3 clilib -c show_source_code=False --html -o html
+pip3 install git+https://github.com/gageleblanc/pdoc-rest.git
+pdoc clilib -c show_source_code=False --html -o html
 docker build --no-cache -t gleblanc/clilib-docs:latest .
 docker push gleblanc/clilib-docs:latest
